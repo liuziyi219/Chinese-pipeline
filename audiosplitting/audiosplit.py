2d4baba145d07413762f09aa6506ef87a6161e4c
liuziyi219 <49853156+liuziyi219@users.noreply.github.com>
2019-06-01 14:16:12 +0800

    Update and rename audiospilitting/example.py to audiosplitting/audiosplit.py

--- audiosplitting/audiosplit.py
+++ audiosplitting/audiosplit.py
@@ -62,31 +62,7 @@ def frame_generator(frame_duration_ms, audio, sample_rate):
 
 def vad_collector(sample_rate, frame_duration_ms,
                   padding_duration_ms, vad, frames):
-    """Filters out non-voiced audio frames.
-
-    Given a webrtcvad.Vad and a source of audio frames, yields only
-    the voiced audio.
-
-    Uses a padded, sliding window algorithm over the audio frames.
-    When more than 90% of the frames in the window are voiced (as
-    reported by the VAD), the collector triggers and begins yielding
-    audio frames. Then the collector waits until 90% of the frames in
-    the window are unvoiced to detrigger.
-
-    The window is padded at the front and back to provide a small
-    amount of silence or the beginnings/endings of speech around the
-    voiced frames.
-
-    Arguments:
-
-    sample_rate - The audio sample rate, in Hz.
-    frame_duration_ms - The frame duration in milliseconds.
-    padding_duration_ms - The amount to pad the window, in milliseconds.
-    vad - An instance of webrtcvad.Vad.
-    frames - a source of audio frames (sequence or generator).
-
-    Returns: A generator that yields PCM audio data.
-    """
+  
     num_padding_frames = int(padding_duration_ms / frame_duration_ms)
     # We use a deque for our sliding window/ring buffer.
     ring_buffer = collections.deque(maxlen=num_padding_frames)
@@ -102,9 +78,7 @@ def vad_collector(sample_rate, frame_duration_ms,
         if not triggered:
             ring_buffer.append((frame, is_speech))
             num_voiced = len([f for f, speech in ring_buffer if speech])
-            # If we're NOTTRIGGERED and more than 90% of the frames in
-            # the ring buffer are voiced frames, then enter the
-            # TRIGGERED state.
+          
             if num_voiced > 0.9 * ring_buffer.maxlen:
                 triggered = True
                 sys.stdout.write('+(%s)' % (ring_buffer[0][0].timestamp,))
@@ -147,9 +121,9 @@ def main(args):
     vad = webrtcvad.Vad(int(args[0]))
     frames = frame_generator(30,audio,sample_rate)
     frames = list(frames)
-    segments = vad_collector(sample_rate, 30, 280, vad, frames)
+    segments = vad_collector(sample_rate, 30, 300, vad, frames)
     for i, segment in enumerate(segments):
-        path = 'result/xinwenlianbo280/xinwenlianbo-%002d.wav' % (i,)
+        path = 'filename-%002d.wav' % (i,)
         print(' Writing %s' % (path,))
         write_wave(path, segment, sample_rate)
 
