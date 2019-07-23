45389e7bb991a968e22b365e4e3867c52421a52c
liuziyi219 <49853156+liuziyi219@users.noreply.github.com>
2019-07-23 23:09:11 +0800

    Update and rename xunfei.py to xunfeisdk/xunfei.py

--- xunfeisdk/xunfei.py
+++ xunfeisdk/xunfei.py
@@ -11,22 +11,17 @@ import wave
 import re
 import sys
 
- # 调用动态链接库  
-dll = cdll.LoadLibrary( "libs/x64/libmsc.so")  
- #登录参数，apppid一定要和你的下载SDK对应  
-login_params = b"appid = 5d162b2b, work_dir = ."  
-#login_params = stringutils.getLoginParams()
+ 
+dll = cdll.LoadLibrary( "YOUR_DIR/libmsc.so")  
+login_params = b"appid = YOUR APPID, work_dir = YOUR WORK_DIR"  
    
 FRAME_LEN = 640  # Byte  
    
 MSP_SUCCESS = 0  
-# 返回结果状态  
 MSP_AUDIO_SAMPLE_FIRST = 1  
 MSP_AUDIO_SAMPLE_CONTINUE = 2  
 MSP_AUDIO_SAMPLE_LAST = 4  
-MSP_REC_STATUS_COMPLETE = 5  
- # 你的语音文件路径  
-filename = "example.wav"  
+MSP_REC_STATUS_COMPLETE = 5    
    
 class Msp:  
     def __init__(self):  
@@ -34,21 +29,15 @@ class Msp:
    
     def login(self):  
         ret = dll.MSPLogin(None, None, login_params)  
-        # print('MSPLogin =>', ret)  
    
     def logout(self):  
-        ret = dll.MSPLogout()  
-        # print('MSPLogout =>', ret)  
+        ret = dll.MSPLogout() 
    
     def isr(self, audiofile, session_begin_params):  
         ret = c_int()  
         sessionID = c_voidp()  
         dll.QISRSessionBegin.restype = c_char_p  
-        sessionID = dll.QISRSessionBegin(None, session_begin_params, byref(ret))  
-        print('QISRSessionBegin => sessionID:', sessionID, 'ret:', ret.value)  
-   
-        # 每秒【1000ms】  16000 次 * 16 bit 【20B】 ，每毫秒：1.6 * 16bit 【1.6*2B】 = 32Byte  
-        # 1帧音频20ms【640B】 每次写入 10帧=200ms 【6400B】  
+        sessionID = dll.QISRSessionBegin(None, session_begin_params, byref(ret))   
    
         piceLne = FRAME_LEN * 20  
        # piceLne = 1638*2  
@@ -57,18 +46,9 @@ class Msp:
    
         wavfile = open(audiofile,'rb')
         wavData = wavfile.read(piceLne)
-
-       # wavData = wData[0].tostring()
         ret = dll.QISRAudioWrite(sessionID, wavData, len(wavData), MSP_AUDIO_SAMPLE_FIRST, byref(epStatus), byref(recogStatus)) 
         print('len(wavData):', len(wavData), 'QISRAudioWrite ret:', ret, 'epStatus:', epStatus.value, 'recogStatus:', recogStatus.value)  
- 
-      #  for index in range(1, len(wData)):
-       #     wavData = wData[index].tostring()
-        #    if len(wavData) == 0:  
-         #       break
-          #  ret = dll.QISRAudioWrite(sessionID, wavData, len(wavData), MSP_AUDIO_SAMPLE_CONTINUE, byref(epStatus), byref(recogStatus))  
-           # print('len(wavData):', len(wavData), 'QISRAudioWrite ret:', ret, 'epStatus:', epStatus.value, 'recogStatus:', recogStatus.value)  
-       # time.sleep(0.01)  
+	
         while wavData:
              wavData = wavfile.read(piceLne)
              if len(wavData) == 0:
@@ -76,11 +56,11 @@ class Msp:
              ret = dll.QISRAudioWrite(sessionID, wavData,len(wavData), MSP_AUDIO_SAMPLE_CONTINUE, byref(epStatus), byref(recogStatus))
              
              time.sleep(0.1)
-       # wavfile.close()
+      
         ret = dll.QISRAudioWrite(sessionID, None, 0, MSP_AUDIO_SAMPLE_LAST, byref(epStatus), byref(recogStatus))  
         print("所有待识别音频已全部发送完毕，等待获取识别结果")  
    
-        # -- 获取音频  
+        
         laststr = ''  
         counter = 0  
         while recogStatus.value != MSP_REC_STATUS_COMPLETE:  
@@ -89,15 +69,14 @@ class Msp:
             retstr = dll.QISRGetResult(sessionID, byref(recogStatus), 0, byref(ret))  
             if retstr is not None:  
                 laststr += retstr.decode()  
-                # print(laststr)  
-            # print('ret:', ret.value, 'recogStatus:', recogStatus.value)  
+                
             counter += 1  
             time.sleep(0.2)  
             counter += 1  
 	   
         print(laststr)  
         ret = dll.QISRSessionEnd(sessionID, "normal end")  
-        # print('end ret: ', ret)  
+        
         return laststr  
 
 def XF_text(audiofile, audiorate,resultdir):  
@@ -140,5 +119,5 @@ def main(args):
                 fp.close()
 if __name__=='__main__':
    main(sys.argv[1:])
-#main()
+
 
