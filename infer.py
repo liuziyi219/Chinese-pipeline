4f0190f362b70f21fc3ed3dae9452eb508efb272
liuziyi219 <49853156+liuziyi219@users.noreply.github.com>
2019-08-17 22:16:58 -0500

    Add files via upload

---
+++ infer.py
@@ -0,0 +1,174 @@
+# -*- coding: UTF-8 -*-
+"""Inferer for DeepSpeech2 model."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import argparse
+import functools
+import paddle.v2 as paddle
+from data_utils.data import DataGenerator
+from model_utils.model import DeepSpeech2Model
+from utils.error_rate import wer, cer
+from utils.utility import add_arguments, print_arguments
+import datetime
+import json
+parser = argparse.ArgumentParser(description=__doc__)
+add_arg = functools.partial(add_arguments, argparser=parser)
+# yapf: disable
+add_arg('batch_size',       int,    10,     "# of samples to infer.")
+add_arg('trainer_count',    int,    1,      "# of Trainers (CPUs or GPUs).")
+add_arg('beam_size',        int,    500,    "Beam search width.")
+add_arg('num_proc_bsearch', int,    8,      "# of CPUs for beam search.")
+add_arg('num_conv_layers',  int,    2,      "# of convolution layers.")
+add_arg('num_rnn_layers',   int,    3,      "# of recurrent layers.")
+add_arg('rnn_layer_size',   int,    2048,   "# of recurrent cells per layer.")
+add_arg('alpha',            float,  2.5,    "Coef of LM for beam search.")
+add_arg('beta',             float,  0.3,    "Coef of WC for beam search.")
+add_arg('cutoff_prob',      float,  1.0,    "Cutoff probability for pruning.")
+add_arg('cutoff_top_n',     int,    40,     "Cutoff number for pruning.")
+add_arg('use_gru',          bool,   False,  "Use GRUs instead of simple RNNs.")
+add_arg('use_gpu',          bool,   True,   "Use GPU or not.")
+add_arg('share_rnn_weights',bool,   True,   "Share input-hidden weights across "
+                                            "bi-directional RNNs. Not for GRU.")
+add_arg('infer_manifest',   str,
+        'data/aishell/manifest.test',
+        "Filepath of manifest to infer.")
+add_arg('mean_std_path',    str,
+        'data/aishell/mean_std.npz',
+        "Filepath of normalizer's mean & std.")
+add_arg('vocab_path',       str,
+        'data/aishell/vocab.txt',
+        "Filepath of vocabulary.")
+add_arg('lang_model_path',  str,
+        'models/lm/zhidao_giga.klm',
+        "Filepath for language model.")
+add_arg('model_path',       str,
+        './checkpoints/librispeech/params.latest.tar.gz',
+        "If None, the training starts from scratch, "
+        "otherwise, it resumes from the pre-trained model.")
+add_arg('decoding_method',  str,
+        'ctc_beam_search',
+        "Decoding method. Options: ctc_beam_search, ctc_greedy",
+        choices = ['ctc_beam_search', 'ctc_greedy'])
+add_arg('output_file',  str,
+        '',
+        "The output file you want to place."
+       )
+add_arg('input_file',str,
+       '',
+       "The default title from the text in dataset"
+       )
+add_arg('specgram_type',    str,
+        'linear',
+        "Audio feature type. Options: linear, mfcc.",
+        choices=['linear', 'mfcc'])
+# yapf: disable
+args = parser.parse_args()
+
+
+def infer():
+    """extract the duration from manifest"""
+    f=open(args.infer_manifest)
+    timelist=[]
+    for line in f:
+       d=json.loads(line.strip())['duration']
+       timelist.append(d)
+    """Inference for DeepSpeech2."""
+    data_generator = DataGenerator(
+        vocab_filepath=args.vocab_path,
+        mean_std_filepath=args.mean_std_path,
+        augmentation_config='{}',
+        specgram_type=args.specgram_type,
+        num_threads=1,
+        keep_transcription_text=True)
+    batch_reader = data_generator.batch_reader_creator(
+        manifest_path=args.infer_manifest,
+        batch_size=args.batch_size,
+        min_batch_size=1,
+        sortagrad=False,
+        shuffle_method=None) 
+    ds2_model = DeepSpeech2Model(
+        vocab_size=data_generator.vocab_size,
+        num_conv_layers=args.num_conv_layers,
+        num_rnn_layers=args.num_rnn_layers,
+        rnn_layer_size=args.rnn_layer_size,
+        use_gru=args.use_gru,
+        pretrained_model_path=args.model_path,
+        share_rnn_weights=args.share_rnn_weights)
+
+    # decoders only accept string encoded in utf-8
+    vocab_list = [chars.encode("utf-8") for chars in data_generator.vocab_list]
+    if args.decoding_method == "ctc_greedy":
+        ds2_model.logger.info("start inference ...")
+        probs_split = ds2_model.infer_batch_probs(infer_data=infer_data,
+            feeding_dict=data_generator.feeding)
+        result_transcripts = ds2_model.decode_batch_greedy(
+            probs_split=probs_split,
+            vocab_list=vocab_list)
+    else:
+        ds2_model.init_ext_scorer(args.alpha, args.beta, args.lang_model_path,
+                                  vocab_list)
+        ds2_model.logger.info("start inference ...")
+
+
+        with open(args.input_file,'r') as f:
+                l = f.readlines()
+                l[8] = "ASR_01|CMN\n"
+                start_time = l[10].split('|')[0]
+                end_time = l[10].split('|')[1]
+                time_now = str(datetime.datetime.now())[:16] # get the current time
+                l[10] = "|".join(["ASR_01",time_now,"Source_Program=Baidu DeepSpeech2,infer.sh","Source_Person=Zhaoqing Xu,Shuwei Xu","Codebook=Chinese Speech to Text\n"])
+                end_line = ""
+                if l[-1].startswith("END"):
+                      end_line = l[-1]
+                l = l[:11]
+
+        with open(args.output_file,"w") as f:
+               f.writelines(l)
+ 
+
+        for infer_data in batch_reader():
+	    probs_split = ds2_model.infer_batch_probs(infer_data=infer_data,
+               feeding_dict=data_generator.feeding)
+            result_transcripts = ds2_model.decode_batch_beam_search(
+              probs_split=probs_split,
+              beam_alpha=args.alpha,
+              beam_beta=args.beta,
+              beam_size=args.beam_size,
+              cutoff_prob=args.cutoff_prob,
+              cutoff_top_n=args.cutoff_top_n,
+              vocab_list=vocab_list,
+              num_processes=args.num_proc_bsearch)
+            for result in result_transcripts:
+
+              with open(args.output_file,'a+') as f:
+                 print("\nOutput Transcription: %s" %
+                 result.encode('utf-8'))
+            #	 try:
+             #    print(start_time)
+                 index=0
+                 start,m_sec = start_time.split('.')
+                 time_format = '%Y%m%d%H%M%S'
+                 end = (datetime.datetime.strptime(start,time_format) + datetime.timedelta(0,timelist[index])).strftime(time_format)
+                 index+=1
+                 prefix = start + '.' + m_sec +  '|' + end + '.' + m_sec + '|ASR_01|'
+                 f.write(prefix)			
+                 f.write(result.encode('utf-8'))
+                 f.write('\n')
+                 start_time = end + '.' + m_sec
+                # except:
+                 #    continue
+        with open(args.output_file, 'a+') as f:
+             f.write(end_line)
+        ds2_model.logger.info("finish inference")
+
+def main():
+   # print_arguments(args)
+    paddle.init(use_gpu=args.use_gpu,
+                rnn_use_batch=True,
+                trainer_count=args.trainer_count)
+    infer()
+
+
+if __name__ == '__main__':
+    main()
---
+++ infer.sh
@@ -0,0 +1,141 @@
+#
+# Chinese pipeline: extract text from speech
+#
+# Changelog
+#
+#	2018-12-07 The words date and file are a reserved in bash, use DAT and FIL for variables; check for completed files (FFS)
+#       2019-08-02 Change the audio spliting method; add audiosplit.py as VAD method for splitting videos
+
+
+# exit when any command fails
+#set -e
+
+
+
+echo "\n\tStarting infer.sh for automated speech recognition ...\n"
+
+cd ..
+PWDDIR=$( pwd ) # The directory of /code 
+BASEDIR='/mnt/rds/redhen/gallina'
+FAILED="/tmp/check-recordings-daemon-sysdown"
+TO=`cat $BASEDIR/Singularity/Chinese_Pipeline/e-mail`
+
+# Move to the main tv storage directory N days ago and list the contents
+if [ -z "$1" ] ; then DAY=0 ; else DAY=${1:0:10} ; fi
+if [ "$( echo "$1" | egrep '^[0-9]+$' )" ] ; then DAY="$1"
+  elif [ "${#1}" -eq "7" ] ; then cd $BASEDIR/tv/${1%-*}/$1 ; DAY=""
+  elif [ "$1" = "here" ] ; then DAY="$( pwd )" DAY=${DAY##*/} DAY="$[$[$(date +%s)-$(date -d "$DAY" +%s)]/86400]"
+  elif [ "$1" = "+" ] ; then DAY=`pwd` ; DAY=${DAY##*/}
+    DAY="$[$[$(date +%s)-$(date -ud "$DAY" +%s)]/86400]" ; DAY=$[DAY-$2]
+  elif [ "$1" = "-" ] ; then DAY=`pwd` ; DAY=${DAY##*/}
+    DAY="$[$[$(date +%s)-$(date -ud "$DAY" +%s)]/86400]" ; DAY=$[DAY+$2]
+  elif [ "${#DAY}" -eq "10" ] ; then DAY="$[$[$(date +%s)-$(date -ud "$DAY" +%s)]/86400]"
+  else echo "$1?"
+fi #;  echo "DAY is $DAY ; 1 is $1 ; 2 is $2"
+     
+#if [ -n "$DAY" ] ; then DIR="$BASEDIR/tv/$(date -ud "-$DAY day" +%Y)/$(date -ud "-$DAY day" +%Y-%m)/$(date -ud "-$DAY day" +%F)" ; fi 
+DIR="$BASEDIR/tv/2019/2019-05/2019-05-03"
+if [ -d $DIR ] ; then cd $DIR ; else echo "No $DIR" ; exit ; fi
+
+echo "\tWorking on `pwd`\n"
+
+DAT=$(basename `pwd`) # get the date name of this day
+MONTH=${DAT:0:7}
+YEAR=${DAT:0:4}
+
+# Generate a list of files to process -- make sure to exclude _CN_ files in English
+# Using find $DIR will generate a list with full path -- find . a list without path
+find . -name '*_CN_*.mp4' ! -iname "*CGTN*" -exec cp {} $PWDDIR/temp_data/ \;
+
+# Working directory -- it's unclear we really need to copy the files -- a downside is that unprocessed files remain in this location
+cd $PWDDIR/temp_data
+
+# If there are no files for a particular day, alert us with an e-mail
+if [ -z "$(ls -A $DAT*.mp4)" ]; then
+       which mail
+       echo -e "\n\tInfer.sh reports that $DAT doesn't have any Chinese files.\n\tPlease intervene as needed." > $FAILED
+       /usr/bin/mailx -s "No Chinese files on $DAT\n" $TO < $FAILED
+       exit 0
+fi
+
+# Initialize counters
+n=0 m=0
+
+for FIL in $DAT*.mp4 ; do n=$[n+1]
+
+  # Skip existing files
+#  if [ -f "$PWDDIR/new_text/$YEAR/$MONTH/$DAT/${FIL%.*}.txt" ] ; then echo -e "\t${FIL%.*}.txt has already been processed" ; m=$[m+1] ; continue ; fi
+
+  # Extract and split the a32000 {FIL%%.*}.wav
+  ffmpeg -i $FIL -ac 1 -ar 32000 ${FIL%%.*}.wav
+  mkdir -p ${FIL%%.*}
+  # Use VAD to split the whole audio into piece
+  python ../code/audiosplit.py \
+    --target_dir=$PWDDIR/temp_data/${FIL%%.*}.wav \
+    --output_dir=$PWDDIR/temp_data/${FIL%%.*}
+  
+  rm ${FIL%%.*}.wav
+  rm $FIL
+
+  # For all the pieces that are longer than 30 seconds, split them again
+  python ../code/audiosplit.py \
+    --target_dir=$PWDDIR/temp_data/${FIL%%.*}  --output_dir=$PWDDIR/temp_data/${FIL%%.*}
+  echo $FIL' split completed' 
+done
+
+# Completed
+if [ "$m" -eq "$n" ] ; then exit ; fi
+
+# Create manifests
+for FIL in `ls -d $DAT*` ; do
+
+   # Skip existing files
+   if [ -f $PWDDIR'/new_text/'$YEAR/$MONTH/$DAT/${FIL%.*}.txt ] ; then echo -e "\tSkipping manifest for $FIL" ; continue ; fi
+
+   python ../code/manifest.py \
+     --target_dir=$PWDDIR/temp_data/$FIL  \
+     --manifest_path=$PWDDIR/temp_manifest/$FIL
+done
+
+cd $PWDDIR/temp_manifest
+
+# Run the automated speech-to-text python script
+for manifest in $DAT* ; do
+	 echo -e "\n\tRunning ASR on $manifest ...\n"
+         mkdir -p $PWDDIR'/new_text/'$YEAR/$MONTH/$DAT
+         CUDA_VISIBLE_DEVICES=0 \
+         python -u ../code/infer.py \
+            --batch_size=10 \
+            --trainer_count=1 \
+            --beam_size=300 \
+            --num_proc_bsearch=2 \
+            --num_conv_layers=2 \
+            --num_rnn_layers=3 \
+            --rnn_layer_size=1024 \
+            --alpha=2.4 \
+            --beta=5.0 \
+            --cutoff_prob=0.99 \
+            --cutoff_top_n=40 \
+            --use_gru=True \
+            --use_gpu=True \
+            --share_rnn_weights=False \
+            --infer_manifest=$PWDDIR'/temp_manifest/'$manifest \
+            --mean_std_path=$BASEDIR'/models/mean_std.npz' \
+            --vocab_path=$BASEDIR'/models/vocab.txt' \
+            --model_path=$BASEDIR'/models/params.tar.gz'  \
+            --lang_model_path=$BASEDIR'/models/zhidao_giga.klm' \
+            --decoding_method='ctc_beam_search' \
+            --specgram_type='linear' \
+            --output_file=$PWDDIR'/new_text/'$YEAR/$MONTH/$DAT/$manifest'.txt' \
+            --input_file=$BASEDIR'/tv/'$YEAR/$MONTH/$DAT/$manifest'.txt'
+            if [ $? -ne 0 ]; then
+               echo "Failed in inference!"
+               rm $PWDDIR'/new_text/'$YEAR/$MONTH/$DAT/$manifest'.txt'
+               continue
+            fi
+            echo $manifest' is done'
+            #rm $manifest
+            #rm -rf $PWDDIR/temp_data/$manifest
+done
+
+exit 0
